ff9dc3e506937c08c375283b096d2451dd15ed65
Arturo Céspedes Pedrazas <cespedes.peart22@triana.salesianos.edu>
2023-01-14 00:40:58 +0100

    cambios, grid e imagenes

--- proyectoflutter/lib/main.dart
+++ proyectoflutter/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Pokemon List'),
         ),
-        
         body: PokemonList(),
       ),
     );
@@ -43,17 +42,34 @@ class _PokemonListState extends State<PokemonList> {
       future: _pokemon,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return ListView.builder(
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, //number of columns
+              childAspectRatio: 1.5,
+            ),
             itemCount: snapshot.data?.length,
             itemBuilder: (context, index) {
               final pokemon = snapshot.data![index];
-              return ListTile(
-                leading: Image.network(pokemon.imageUrl),
-                title: Text(pokemon.name),
-                onTap: () {
-                  Navigator.of(context)
-                      .pushNamed('/pokemon', arguments: pokemon);
-                },
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                      pokemon.imageUrl,
+                      width: 300,
+                      height: 300,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(pokemon.name.substring(0, 1).toUpperCase() +
+                        pokemon.name.substring(1)),
+                    TextButton(
+                      child: Text("Ver detalles"),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed('/pokemon', arguments: pokemon);
+                      },
+                    ),
+                  ],
+                ),
               );
             },
           );
@@ -70,110 +86,93 @@ class Pokemon {
   final int id;
   final String name;
   final String imageUrl;
+  final double weight;
+  final double height;
+  final List<String> types;
+  final List<String> abilities;
 
-  Pokemon({required this.id, required this.name, required this.imageUrl});
+  Pokemon(
+      {required this.id,
+      required this.name,
+      required this.imageUrl,
+      required this.weight,
+      required this.height,
+      required this.types,
+      required this.abilities});
 }
 
 Future<List<Pokemon>> getPokemon() async {
-  final response =
-      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=10'));
-  if (response.statusCode != 200) {
-    throw Exception('Error al obtener los Pokemon');
-  }
-  final data = jsonDecode(response.body) as Map<String, dynamic>;
-  final pokemonList = data['results'] as List<dynamic>;
-  final pokemon = <Pokemon>[];
-  for (final pokemonData in pokemonList) {
-    final pokemonResponse = await http.get(Uri.parse(pokemonData['url']));
-    final pokemonJson =
-        jsonDecode(pokemonResponse.body) as Map<String, dynamic>;
-    final imageUrl = pokemonJson['sprites']['front_default'];
-    if (imageUrl == null || imageUrl.isEmpty) {
-    } else {
+  try {
+    final response =
+        await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=151'));
+    if (response.statusCode != 200) {
+      throw Exception('Error al obtener los Pokemon');
+    }
+    final data = jsonDecode(response.body) as Map<String, dynamic>;
+    final pokemonList = data['results'] as List<dynamic>;
+    final pokemon = <Pokemon>[];
+    for (final pokemonData in pokemonList) {
+      final pokemonResponse = await http.get(Uri.parse(pokemonData['url']));
+      final pokemonJson =
+          jsonDecode(pokemonResponse.body) as Map<String, dynamic>;
+      final imageUrl = pokemonJson['sprites']['front_default'];
+      if (imageUrl == null || imageUrl.isEmpty) {
+        continue;
+      }
       final uri = Uri.tryParse(imageUrl);
       if (uri == null) {
-      } else {
-        final currentPokemon = Pokemon(
-          id: pokemonJson['id'],
-          name: pokemonJson['name'],
-          imageUrl: imageUrl,
-        );
-        pokemon.add(currentPokemon);
+        continue;
       }
+      final weight = pokemonJson['weight'] / 10;
+      final height = pokemonJson['height'] / 10;
+      final types = (pokemonJson['types'] as List<dynamic>)
+          .map((type) => type['type']['name'])
+          .cast<String>()
+          .toList();
+      final abilities = (pokemonJson['abilities'] as List<dynamic>)
+          .map((ability) => ability['ability']['name'])
+          .cast<String>()
+          .toList();
+      final id = pokemonJson['id'];
+      pokemon.add(Pokemon(
+          id: id,
+          name: pokemonData['name'],
+          imageUrl: imageUrl,
+          weight: weight,
+          height: height,
+          types: types,
+          abilities: abilities));
     }
+    return pokemon;
+  } catch (e) {
+    throw Exception('Error al obtener los Pokemon');
   }
-  return pokemon;
 }
 
 class PokemonDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final pokemon = ModalRoute.of(context)!.settings.arguments as Pokemon;
+    final Pokemon pokemon =
+        ModalRoute.of(context)!.settings.arguments as Pokemon;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pokemon Details'),
+        title: Text(pokemon.name),
       ),
-      body: PokemonDetails(pokemon: pokemon),
+      body: Column(
+        children: <Widget>[
+          Image.network(pokemon.imageUrl, width: 400,
+                      height: 400,
+                      fit: BoxFit.cover,),
+          SizedBox(height: 16),
+          Text("Altura: ${pokemon.height}m"),
+          SizedBox(height: 16),
+          Text("Peso: ${pokemon.weight}kg"),
+          SizedBox(height: 16),
+          Text("Tipos: ${pokemon.types.join(', ')}"),
+          SizedBox(height: 16),
+          Text("Habilidades: ${pokemon.abilities.join(', ')}"),
+        ],
+      ),
     );
   }
-}
-
-class PokemonDetails extends StatefulWidget {
-  final Pokemon pokemon;
-  PokemonDetails({required this.pokemon});
-
-  @override
-  _PokemonDetailsState createState() => _PokemonDetailsState();
-}
-
-class _PokemonDetailsState extends State<PokemonDetails> {
-  Future<Map<String, dynamic>>? _pokemon;
-
-  @override
-  void initState() {
-    super.initState();
-    _pokemon = getPokemonDetails(widget.pokemon.id);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Map<String, dynamic>>(
-      future: _pokemon,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final pokemon = snapshot.data;
-          return Column(
-            children: <Widget>[
-              // Imagen del pokemón
-              Image.network(widget.pokemon.imageUrl),
-              // Nombre del pokemón
-              Text(
-                  'Nombre: ${widget.pokemon.name.toUpperCase()[0] + widget.pokemon.name.substring(1)}'),
-              // ID del pokémon
-              Text('ID : ${pokemon?['id']}'),
-              // Peso del pokemón
-              Text('Peso : ${pokemon?['weight']}'),
-              //Estatura del pokemón
-              Text('Altura : ${pokemon?['height']}'),
-              //Tipo de pokemón
-              Text(
-                  'Tipos: ${pokemon?['types'].map((t) => t['type']['name']).join(', ')}'),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        return CircularProgressIndicator();
-      },
-    );
-  }
-}
-
-Future<Map<String, dynamic>> getPokemonDetails(int id) async {
-  final response =
-      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$id'));
-  if (response.statusCode != 200) {
-    throw Exception('Error al obtener los detalles del Pokemon');
-  }
-  return jsonDecode(response.body) as Map<String, dynamic>;
 }
